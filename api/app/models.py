92bafe8aedf7397eb152f1403fb76f64b63bda84
mehdi <mehdigour20@gmail.com>
2026-09-09 20:36:04 +0100

    phase-6.1: add password_hash to sellers

--- api/app/models.py
+++ api/app/models.py
@@ -11,6 +11,7 @@ class Seller(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
